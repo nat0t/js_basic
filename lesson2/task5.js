83a1258dca675ad2b0fcb8f2c2a4f9d7a12a2da9
nat0t <nataniel1987@gmail.com>
2020-12-27 18:06:46 +0300

    Added tasks for lesson2

---
+++ lesson2/task5.js
@@ -0,0 +1,17 @@
+/* Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return. */
+
+function sum(x, y) {
+    return x + y;
+}
+
+function sub(x, y) {
+    return x - y;
+}
+
+function mult(x, y) {
+    return x * y;
+}
+
+function div(x, y) {
+    return x / y;
+}
